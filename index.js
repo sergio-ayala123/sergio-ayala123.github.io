f14bbc1c714535647a5931683feb621f918ca7a1
Sergio <venturian6@gmail.com>
2020-04-19 15:18:20 -0600

    added extra text field
    another text field

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function() {
     item.addEventListener("click", function(e){
 
 
-        if(e.target.nodeName.toLowerCase() == 'button'){
+        if(e.target.nodeName.toLowerCase() == 'input'){
             var clicked = e.target;
             alert(clicked);
         }
@@ -13,19 +13,29 @@ window.addEventListener("load", function() {
 
 });
 
+var d = new Date();
+
+
+
 function addRow() {
     var table = document.getElementById("center");
     var x = document.getElementById("item").value
+    var t = document.getElementById("time")
+
+
+    
 
     if(x.length == 0){
-        alert("Field is empty");
+       // alert(d.getHours());
+        alert(d.getMinutes());
+
     }
     else {
+        
         var row = table.insertRow(table.length);
         var cell = row.insertCell(table.length);
-        cell.innerHTML = x + "<button type = 'button'>X</button>";
+        cell.innerHTML = x + " <button >X</button><br>" + t.value;
+        
     }
-
-
 }
 
