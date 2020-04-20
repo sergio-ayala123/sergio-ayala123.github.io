818ec04a84bc637eb976774432e4e69ab9daf49c
Sergio <venturian6@gmail.com>
2020-04-19 18:46:48 -0600

    fix
    fix

--- index.js
+++ index.js
@@ -22,25 +22,22 @@ var d = new Date();
 
 
 
+
+
 function addRow() {
     var table = document.getElementById("center");
     var x = document.getElementById("item").value
     var t = document.getElementById("time")
 
-
-    
-
     if(x.length == 0){
        // alert(d.getHours());
         alert(d.getMinutes());
-
-    }
-    else {
+    }  
+    else {       
         
         var row = table.insertRow(table.length);
         var cell = row.insertCell(table.length);
-        cell.innerHTML = x + " <button >X</button><br>" + t.value;
-        
+        cell.innerHTML = x + " <button >X</button><br>" + t.value;        
     }
 }
 
